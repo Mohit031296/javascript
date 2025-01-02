1656b07298fe4a55f394d39e0d9e9ca334f5566d
Mohit031296 <mm303074@gmail.com>
2025-01-02 19:47:41 +0000

    done

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,9 +1,15 @@
-Const CustomerID = 3442
+const CustomerID = 3442
 let CustomerEmail = 'mm30303@gmail.com'
 var CustomerName = 'Rahul'
 CustomerCity = 'savarkundla'
 
-// CustomerID = 211
+// CustomerID = 211 // const is not allowed to change value
 
- console.log(CustomerID);
+CustomerEmail = 123@outer.com
+CustomerName = 'Mohit C'
+CustomerCity = 'Rampur'
 
+// console.log(CustomerID);
+
+
+console.table([CustomerID, CustomerEmail, CustomerName, CustomerCity])
