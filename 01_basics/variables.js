3aa925463471a10dec5a865b261e926deec438cb
Mohit031296 <mm303074@gmail.com>
2025-01-02 19:41:39 +0000

    done

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,1 +1,9 @@
-Const CustomerID = 
+Const CustomerID = 3442
+let CustomerEmail = 'mm30303@gmail.com'
+var CustomerName = 'Rahul'
+CustomerCity = 'savarkundla'
+
+CustomerID = 211
+
+// console.log(CustomerID);
+
