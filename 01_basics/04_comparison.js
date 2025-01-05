21f70e50ff3a8ac360c8c6d39aed5a9b95692977
Mohit031296 <mm303074@gmail.com>
2025-01-05 05:25:37 +0000

    done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// There is no issue in the above code as the datatype is the same = Number
+
+// console.log("2"> 1);
+// It will create issue as the 1st is String and 2nd is Number. Maybe it shows the correct output but we need to avode this type of use.
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// There is issue in javascript. Becuase sometime it will convert to 0 or NaN. 
+
+
+// Use Strict Equality using ===
+// and Strinct Inequality using !==
+
+console.log("1" === 1);
---
+++ 01_basics/Datatype-summary.js
@@ -0,0 +1,50 @@
+/* =============Primitive============= */
+
+// String
+const Name = "Mohit"
+
+
+//Symbol
+let id = Symbol('123')
+let id2 = Symbol('123')
+/* It seems like both are same but actually it is not the same. */
+
+// Number and BigInt
+let Score = 80.12221
+let ScoreBig = 9012221112234954795473n
+
+// Null
+let IsLoggedIn = null
+
+// Boolean
+let IsFieldEmpty = true /* Return boolean */
+
+// Undefined
+let x /* Return undefined */
+
+// console.log(id === id2)
+// console.log(typeof Name)
+// console.log(typeof id)
+// console.log(typeof Score)
+// console.log(typeof ScoreBig)
+// console.log(typeof IsLoggedIn)
+// console.log(typeof IsFieldEmpty)
+// console.log(typeof x)
+
+
+/* =============Non-Primitive============= */
+let Heros = ["Mohit", "Deep", "Xyz"]
+
+const StudentInfo = {
+
+    Name: "Mohit",
+    Enrollment: 1
+
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(myFunction)
+console.log(typeof(myFunction))
