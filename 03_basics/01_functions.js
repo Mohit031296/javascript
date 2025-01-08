440a664566281b860a03ca1405f63287cfd458bb
Mohit031296 <mm303074@gmail.com>
2025-01-08 13:27:14 +0000

    function done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,14 +40,74 @@
 // let result = sumofTowNumber(2,2)
 // console.log(`result: ${result}`)
 
-function userStatus(username){
-if(username == undefined) {
+
+/* If not add arguments in the function */
+// function userStatus(username){
+// if(username == undefined) {
     
-    return `Please enter the username.`
-}
-    return `${username} just logged in.`
+//     return `Please enter the username.`
+// }
+//     return `${username} just logged in.`
+// }
+
+// // console.log(userStatus("Mohit"))
+// // console.log(userStatus(""))
+// console.log(userStatus("mohit"))
+
+
+
+
+/* Don't know how many parameteres in the function - for example add to cart section */
+
+/* Take only First Value */
+// function calTwoVal(num1){
+//      return num1;
+// }
+// console.log(calTwoVal(2,3))
+
+
+
+/* Using Rest - It will create array of parameteres */
+// function calTwoVal(...num1){
+//      return num1;
+// }
+// console.log(calTwoVal(2,3,100,400312))
+
+
+/* Using  variable + Rest - It will create array of parameteres */
+// function calTwoVal(val1, val2, ...num1){
+//     return val1, val2, num1;
+// }
+// console.log(calTwoVal(2,3,100,400312)) /* return [100, 400312] */
+
+
+
+/* HOW TO USE object in function */
+
+// const UserDetail = {
+//     Username: "Mohit",
+//     Price: 19
+// }
+
+// function UserFunction(anyobject){
+//     console.log(`Username is ${anyobject.Username} and price is ${anyobject.Price}`)
+// }
+// UserFunction(UserDetail) /* 1st method */
+// UserFunction({
+//     Username: "XZJ",
+//     Price: 122
+// }
+// ) /* 2nd method */
+
+
+
+/* HOW TO USE Array in function */
+
+const UserArray = new Array(1,2,4,5)
+
+function myArrayFunction(anyArray){
+    return anyArray[2]
 }
 
-// console.log(userStatus("Mohit"))
-// console.log(userStatus(""))
-console.log(userStatus("mohit"))
+console.log(myArrayFunction(UserArray)) /* 1st method */
+console.log(myArrayFunction([100,200,500])) /* 2nd method */
