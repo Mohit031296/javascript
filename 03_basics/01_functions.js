52b89b86743c1237bf12b81c489e2ac4e5b3e375
Mohit031296 <mm303074@gmail.com>
2025-01-08 08:47:13 +0000

    function done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+/* Function */
+
+// function myNameFunction(){
+
+//     console.log("M")
+//     console.log("o")
+//     console.log("H")
+//     console.log("I")
+//     console.log("T")
+
+// }
+
+// myNameFunction()
+
+function sumofTowNumber(num1, num2){
+
+    console.log(num1 + num2)
+
+}
+
+sumofTowNumber(3,2) /* Return 5 */
+sumofTowNumber(3,"2") /* Return 32 */
+sumofTowNumber(3, undefined) /* Return NaN */
+sumofTowNumber(3, null) /* Return 3 */
