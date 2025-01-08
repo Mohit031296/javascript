a6595053e9069e27ffe6dc2fe83f187c09ebfa3d
Mohit031296 <mm303074@gmail.com>
2025-01-08 12:02:53 +0000

    done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -12,13 +12,42 @@
 
 // myNameFunction()
 
-function sumofTowNumber(num1, num2){
 
-    console.log(num1 + num2)
+/* Sum of two numbers and datatype results */
 
+// function sumofTowNumber(num1, num2){
+
+//     console.log(num1 + num2)
+
+// }
+
+// sumofTowNumber(3,2) /* Return 5 */
+// sumofTowNumber(3,"2") /* Return 32 */
+// sumofTowNumber(3, undefined) /* Return NaN */
+// sumofTowNumber(3, null) /* Return 3 */
+// let result = sumofTowNumber(3,2)
+// console.log(`Result: ${result}`) /* Result: undefined */
+
+
+/* How to store value in variables */
+// function sumofTowNumber(num1, num2){
+
+//     let result = num1 + num2;
+//     return result;
+//     console.log("It iwll not run") /* After retun nothing will execute */ 
+// }
+
+// let result = sumofTowNumber(2,2)
+// console.log(`result: ${result}`)
+
+function userStatus(username){
+if(username == undefined) {
+    
+    return `Please enter the username.`
+}
+    return `${username} just logged in.`
 }
 
-sumofTowNumber(3,2) /* Return 5 */
-sumofTowNumber(3,"2") /* Return 32 */
-sumofTowNumber(3, undefined) /* Return NaN */
-sumofTowNumber(3, null) /* Return 3 */
+// console.log(userStatus("Mohit"))
+// console.log(userStatus(""))
+console.log(userStatus("mohit"))
