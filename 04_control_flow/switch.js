8debf160f433c05993c768b7005c943475b9f857
Mohit031296 <mm303074@gmail.com>
2025-01-13 03:17:17 +0000

    done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,44 @@
+'use strict'
+
+
+/* Number */
+// const month = 2
+
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//         case 3:
+//             console.log("Mar");
+//             break;        
+//     default:
+//         console.log("Dec")
+//         break;
+// }
+
+/* String */
+const month1 = "Feb"
+
+switch (month1) {
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "Mar":
+        console.log("Mar");
+        break;
+        case "Apr":
+            console.log("Apr");
+            break;        
+    default:
+        console.log("Dec")
+        break;
+}
