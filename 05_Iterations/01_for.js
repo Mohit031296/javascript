aba9cdab7b0120e888c0320b49f6ff3443cef23e
Mohit031296 <mm303074@gmail.com>
2025-01-13 05:36:57 +0000

    done

---
+++ 05_Iterations/01_for.js
@@ -0,0 +1,28 @@
+// For Loop
+
+/* For loop to print 10 numbers and set 5 is the best number */
+// for (let i = 0; i <= 10; i++) {
+//     if(i === 5){
+//         console.log(`${i} is the best number`)
+//     }
+//     console.log(i)  
+// }
+
+
+/* For loop to print Table upto 10 numbers */
+// for(let i = 1; i <= 10; i++){
+
+//     console.log(`Outer Loop value $ {i}`)
+//     for(let j = 1; j<=10; j++){
+//         console.log(`${i} * ${j} = `, + i*j)
+//      }
+
+// }
+
+
+/* For loop in array */
+const myHeros = ["ABC", "XYZ", "LMN"]
+
+for(let i = 0; i <= myHeros.length; i++){
+    console.log(array[i])
+}
