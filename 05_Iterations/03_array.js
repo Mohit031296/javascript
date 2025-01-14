35db05eba3f0aadb7972922ba4d8c36a3774a9c5
Mohit031296 <mm303074@gmail.com>
2025-01-14 11:22:12 +0000

    for of done

--- 05_Iterations/03_array.js
+++ 05_Iterations/03_array.js
@@ -17,18 +17,30 @@
 // }
 
 
+/* Map */
+
+const map = new Map()
+map.set('IN', "India")
+map.set('FR', "France")
+map.set('UK', "United Kingdom")
+map.set('UK', "United Kingdom")
+
+// console.log(map)
+
+for (const [key, value] of map)
+    console.log(key, value)
 
 /*Object*/
-const myHeros = new Object()
-myHeros.firstName = "Mohit"
-myHeros.lastName = "Mehta"
-myHeros.age = 28
-myHeros.isLoggedIn = false
+// const myHeros = new Object()
+// myHeros.firstName = "Mohit"
+// myHeros.lastName = "Mehta"
+// myHeros.age = 28
+// myHeros.isLoggedIn = false
 
-console.log(myHeros);
+// console.log(myHeros);
 
-for (const [key, value] of Object.entries(myHeros)) {
+// for (const [key, value] of Object.entries(myHeros)) {
 
-    console.log(key, value)
+//     console.log(key, value)
     
-}
+// }
