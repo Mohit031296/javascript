cc7f7122bec8e1a40e2b130ccabdc0c193c4294a
Mohit031296 <mm303074@gmail.com>
2025-01-14 12:34:49 +0000

    for in done

---
+++ 05_Iterations/04_forin.js
@@ -0,0 +1,24 @@
+// For in
+
+/* Objects */
+
+// const myStudentInfo = {
+//     firstName: "Rahul",
+//     lastName: "Roy",
+//     math: 75,
+//     science: 92,
+//     physics: 41
+// }
+
+// for(const item in myStudentInfo)
+//     // console.log(item) /* Print key */
+//     console.log(`${item} = ${myStudentInfo[item]}`) /* Print values */
+
+
+const myArrat = new Array("js", "c++", "java", "python")
+
+for (const key in myArrat) {
+    // console.log(key) /* Print key */
+    console.log(`${key} = ${myArrat[key]}`)/* Print values */
+}
+
