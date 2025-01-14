f4e40f19c94689767222fd1fb54c6390e6f9d069
Mohit031296 <mm303074@gmail.com>
2025-01-14 10:34:15 +0000

    while done

---
+++ 05_Iterations/02_while.js
@@ -0,0 +1,42 @@
+// While
+
+// let index = 0
+// while( index <= 10){
+//     console.log(index)
+//     index = index + 2
+// }
+
+
+
+/* Array in while loop */
+
+// const myHeros = ["ABC", "XYZ", "LMN"]
+// let index = 0
+// while(index < myHeros.length){
+//     console.log(myHeros[index])
+//     index++
+// }
+
+
+
+// Do While
+
+let score = 210
+do {
+    console.log(`score is ${score}`)
+    score = score + 1;
+} while (score <= 20);
+
+
+
+/* Break in do while */
+// let score = 10
+// do {
+//     console.log(`score is ${score}`)
+//     score = score + 1;
+//     if (score === 15) {
+//         break;
+//     }
+// } while (score <= 20);
+
+
